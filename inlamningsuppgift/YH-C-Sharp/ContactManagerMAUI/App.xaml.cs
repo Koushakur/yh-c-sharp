942218b0b306541cc919fb438ed308cea7272e1c
Duker <m.oliver.henriksson@gmail.com>
2023-12-12 20:31:36 +0100

    Added MAUI project, default initialization

---
+++ inlamningsuppgift/YH-C-Sharp/ContactManagerMAUI/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ContactManagerMAUI
+{
+    public partial class App : Application
+    {
+        public App() {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ inlamningsuppgift/YH-C-Sharp/ContactManagerMAUI/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ContactManagerMAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell() {
+            InitializeComponent();
+        }
+    }
+}
