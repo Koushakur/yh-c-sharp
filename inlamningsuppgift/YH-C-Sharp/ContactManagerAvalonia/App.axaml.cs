776b4e0a9f07d4d322ffae758c8a49b0163ff655
Duker <m.oliver.henriksson@gmail.com>
2023-12-19 16:23:24 +0100

    Switched from ReactiveUI to CommunityToolkit.MVVM

--- inlamningsuppgift/YH-C-Sharp/ContactManagerAvalonia/App.axaml.cs
+++ inlamningsuppgift/YH-C-Sharp/ContactManagerAvalonia/App.axaml.cs
@@ -1,5 +1,6 @@
 ﻿using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Data.Core.Plugins;
 using Avalonia.Markup.Xaml;
 
 using ContactManagerAvalonia.ViewModels;
@@ -9,24 +10,20 @@ namespace ContactManagerAvalonia;
 
 public partial class App : Application
 {
-    public override void Initialize()
-    {
+    public override void Initialize() {
         AvaloniaXamlLoader.Load(this);
     }
 
-    public override void OnFrameworkInitializationCompleted()
-    {
-        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
-        {
-            desktop.MainWindow = new MainWindow
-            {
+    public override void OnFrameworkInitializationCompleted() {
+        BindingPlugins.DataValidators.RemoveAt(0);
+
+        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop) {
+            desktop.MainWindow = new MainWindow {
                 DataContext = new MainViewModel()
             };
-        }
-        else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
-        {
-            singleViewPlatform.MainView = new MainView
-            {
+
+        } else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform) {
+            singleViewPlatform.MainView = new MainView {
                 DataContext = new MainViewModel()
             };
         }
