3de70bd1ecccaa2f5c93d94fa741305d83c4dd33
Duker <m.oliver.henriksson@gmail.com>
2023-12-21 00:57:20 +0100

    Added preliminary panes for adding and viewing contacts

--- inlamningsuppgift/YH-C-Sharp/ContactManagerAvalonia/ViewModels/MainViewModel.cs
+++ inlamningsuppgift/YH-C-Sharp/ContactManagerAvalonia/ViewModels/MainViewModel.cs
@@ -16,9 +16,9 @@ public partial class MainViewModel : ViewModelBase
 
     public MainViewModel() { }
     public MainViewModel(ContactService contactService, IServiceProvider sp) {
-        _contactService = contactService;
         _sp = sp;
-        ContactListbox = _contactService!.GetContactList().ToList();
+        _contactService = contactService;
+        UpdateList();
     }
 
     #region Observables
@@ -28,39 +28,35 @@ public partial class MainViewModel : ViewModelBase
 
     [ObservableProperty]
     private string _obsTest = "";
-
     [ObservableProperty]
     private string _inputFirstName = "";
-
     [ObservableProperty]
     private string _inputLastName = "";
-
     [ObservableProperty]
     private string _inputEmail = "";
-
     [ObservableProperty]
     private string _inputPhoneNumber = "";
-
     [ObservableProperty]
     private Address _inputAddress = new Address();
+    [ObservableProperty]
+    private bool _paneOutAdd = false;
+    [ObservableProperty]
+    private bool _paneOutView = false;
+    [ObservableProperty]
+    private bool _paneOut3 = false;
+    [ObservableProperty]
+    private object _selectedContact;
+
+    [ObservableProperty]
+    private string _viewFullName = "";
+    [ObservableProperty]
+    private string _viewEmail = "";
+    [ObservableProperty]
+    private string _viewPhoneNumber = "";
 
     #endregion
 
     #region RelayCommands
-
-    [RelayCommand]
-    public void Test() {
-        //Xlist!.Add(new Contact { FirstName = ObsTest, Email = ObsTest });
-        _contactService!.AddContact(new Contact {
-            FirstName = ObsTest,
-            LastName = ObsTest + "Lastname",
-            Email = ObsTest + "@domain.net",
-            PhoneNumber = "000"
-        });
-        _contactService.SaveContactsToFile();
-        ContactListbox = _contactService!.GetContactList().ToList();
-        ObsTest = string.Empty;
-    }
 
     [RelayCommand]
     public void OpenFolder() {
@@ -68,11 +64,42 @@ public partial class MainViewModel : ViewModelBase
     }
 
     [RelayCommand]
+    public void TogglePaneAdd() {
+        PaneOutView = false;
+        PaneOutAdd = true;
+    }
+    [RelayCommand]
     public void AddContact() {
+        var tContact = new Contact {
+            FirstName = InputFirstName,
+            LastName = InputLastName,
+            Email = InputEmail,
+            PhoneNumber = InputPhoneNumber,
+        };
+
+        _contactService!.AddContact(tContact);
+        //_contactService?.SaveContactsToFile();
+        UpdateList();
+
+        InputFirstName = string.Empty;
+        InputLastName = string.Empty;
+        InputEmail = string.Empty;
+        InputPhoneNumber = string.Empty;
+
     }
 
     [RelayCommand]
     public void ViewContact() {
+        if (SelectedContact == null)
+            return;
+
+        PaneOutView = true;
+        PaneOutAdd = false;
+
+        Contact t = (Contact)SelectedContact;
+        ViewFullName = t.FullName;
+        ViewEmail = t.Email;
+        ViewPhoneNumber = t.PhoneNumber;
     }
 
     [RelayCommand]
@@ -83,6 +110,18 @@ public partial class MainViewModel : ViewModelBase
     public void RemoveContact() {
     }
 
+    [RelayCommand]
+    public void TogglePane3() {
+        PaneOut3 = !PaneOut3;
+    }
+
     #endregion
+
+    /// <summary>
+    /// Populate ListBox with all contacts
+    /// </summary>
+    private void UpdateList() {
+        ContactListbox = _contactService!.GetContactList().ToList();
+    }
 
 }
